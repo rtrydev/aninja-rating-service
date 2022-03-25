7fe567e65000a8cf709df737c7a2743055663dbc
rtrydev <r.kulka44@gmail.com>
2022-03-25 18:10:47 +0100

    add repo implementations

--- Repositories/IRatingRepository.cs
+++ Repositories/IRatingRepository.cs
@@ -7,4 +7,13 @@ public interface IRatingRepository
     public Task<IEnumerable<Rating>> GetRatingsForAnime(int animeId);
     public Task<IEnumerable<Rating>> GetRatingsByUser(Guid userId);
     public Task<decimal> GetAverageRatingForAnime(int animeId);
+
+    public Task<Rating?> AddRating(Rating rating);
+    public Task RemoveRating(Rating rating);
+
+    public Task<Anime?> GetAnime(int animeId);
+    public Task<Anime?> AddAnime(Anime anime);
+    public Task<Anime?> UpdateAnime(Anime anime);
+    public Task DeleteAnime(int animeId);
+    public Task<bool> AnimeExists(int animeId);
 }
