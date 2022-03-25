994c68346d50e15301af6fba1af9582ea041cdc3
rtrydev <r.kulka44@gmail.com>
2022-03-25 18:35:10 +0100

    add rating command

---
+++ Handlers/AddRatingCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using aninja_rating_service.Commands;
+using aninja_rating_service.Models;
+using aninja_rating_service.Repositories;
+using MediatR;
+
+namespace aninja_rating_service.Handlers
+{
+    public class AddRatingCommandHandler : IRequestHandler<AddRatingCommand, Rating?>
+    {
+        private IRatingRepository _ratingRepository;
+        public AddRatingCommandHandler(IRatingRepository ratingRepository)
+        {
+            _ratingRepository = ratingRepository;
+        }
+        public async Task<Rating?> Handle(AddRatingCommand request, CancellationToken cancellationToken)
+        {
+            var rating = new Rating()
+            {
+                Id = Guid.NewGuid(),
+                AnimeId = request.AnimeId,
+                SubmissionDate = DateTime.Now,
+                Mark = request.Mark,
+                Comment = request.Comment,
+                SubmitterId = request.SubmitterId
+            };
+
+            var result = await _ratingRepository.AddRating(rating);
+            return result;
+
+        }
+    }
+}
