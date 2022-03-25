d1393ea528bee60e6e74e3549d725c3e5ec5506c
rtrydev <r.kulka44@gmail.com>
2022-03-25 18:29:55 +0100

    add avg ratig get

---
+++ Handlers/GetAverageRatingForAnimeQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using aninja_rating_service.Queries;
+using aninja_rating_service.Repositories;
+using MediatR;
+
+namespace aninja_rating_service.Handlers
+{
+    public class GetAverageRatingForAnimeQueryHandler : IRequestHandler<GetAverageRatingForAnimeQuery, decimal>
+    {
+        private IRatingRepository _ratingRepository;
+        public GetAverageRatingForAnimeQueryHandler(IRatingRepository ratingRepository)
+        {
+            _ratingRepository = ratingRepository;
+        }
+        public async Task<decimal> Handle(GetAverageRatingForAnimeQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _ratingRepository.GetAverageRatingForAnime(request.AnimeId);
+            return result;
+        }
+    }
+}
