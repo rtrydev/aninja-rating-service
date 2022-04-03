04199290fc063cf27e236b6fd832b02129f2eaa4
rtrydev <r.kulka44@gmail.com>
2022-04-03 15:03:02 +0200

    change range to 1 - 10

--- Dtos/RatingAddDto.cs
+++ Dtos/RatingAddDto.cs
@@ -4,7 +4,7 @@ namespace aninja_rating_service.Dtos
 {
     public class RatingAddDto
     {
-        [Range(1, 5)]
+        [Range(1, 10)]
         public decimal Mark { get; set; }
         [MaxLength(500)]
         public string Comment { get; set; }
