663be34d1c3d27ef6381b7a0005ea5a19a9274df
rtrydev <r.kulka44@gmail.com>
2022-04-04 15:28:23 +0200

    add query and its handler for top anime list

---
+++ Handlers/GetTopAnimeQueryHandler.cs
@@ -0,0 +1,23 @@
+using aninja_rating_service.Models;
+using aninja_rating_service.Queries;
+using aninja_rating_service.Repositories;
+using MediatR;
+
+namespace aninja_rating_service.Handlers;
+
+public class GetTopAnimeQueryHandler : IRequestHandler<GetTopAnimeQuery, IEnumerable<Anime>?>
+{
+    private IRatingRepository _ratingRepository;
+
+    public GetTopAnimeQueryHandler(IRatingRepository ratingRepository)
+    {
+        _ratingRepository = ratingRepository;
+    }
+
+    public async Task<IEnumerable<Anime>?> Handle(GetTopAnimeQuery request, CancellationToken cancellationToken)
+    {
+        var animes = await _ratingRepository.GetAnimes();
+        animes = animes.OrderByDescending(async x => await _ratingRepository.GetAverageRatingForAnime(x.ExternalId));
+        return animes;
+    }
+}
